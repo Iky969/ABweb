d6952b57a062835dcc3fb03bdd08bb3cd8b8f3c2
Rzy <168882072+Iky969@users.noreply.github.com>
2026-09-07 13:25:20 +0000

    chore: remove unused boilerplate code files

--- main.rb
+++
@@ -1,9 +0,0 @@
-# Program menghitung total angka 1 sampai 5
-total = 0
-(1..5).each do |i|
-  total += i
-  puts "Menambahkan #{i}, total sementara: #{total}"
-end
-
-puts "Hasil akhir: #{total}"
-# Selesai
--- main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut total = 0;
-    for i in 1..=5 {
-        total += i;
-        println!("Menambahkan {}, total sementara: {}", i, total);
-    }
-    println!("Hasil akhir: {}", total);
-}
-// Selesai
